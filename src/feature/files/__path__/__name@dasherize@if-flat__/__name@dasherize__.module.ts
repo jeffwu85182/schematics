191db2b32c502081be3ae6db4b78eadc19d2e63a
Jeff Wu <crazy.alu@outlook.com>
2018-01-18 13:54:15 +0800

    Remove routing module

--- src/feature/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.module.ts
+++ src/feature/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.module.ts
@@ -2,19 +2,26 @@ import { FeatureLoader } from './feature-loader';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { <%= classify(name) %> } from './<%= dasherize(name) %>';
-import { Router } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { CommonComponentsModule } from '../../components/common-components.module';
 import { TwdComponentsModule } from '../../components/twd-components.module';
-import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module';
 import { FeatureManager } from './feature-manager';
 
 // Optional Module
 import { SharedModule } from 'primeng/primeng';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: <%= classify(name) %>,
+    children: []
+  }
+];
+
 @NgModule({
   imports: [
     CommonModule,
-    <%= classify(name) %>RoutingModule,
+    RouterModule.forChild(routes),
     // fxComponentModule 若是外幣相關
     TwdComponentsModule, // 台幣相關，若是屬於台外幣 Feature 相關則兩個都會用到
     CommonComponentsModule,
