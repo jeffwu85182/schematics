51c76158a65ebd2e2b752685a8a55b4a43111b53
Jeff Wu <crazy.alu@outlook.com>
2018-01-22 14:03:06 +0800

    Add feature component schematic

---
+++ src/feature-component/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit<% if(!!viewEncapsulation) { %>, ViewEncapsulation<% }%><% if(changeDetection !== 'Default') { %>, ChangeDetectionStrategy<% }%> } from '@angular/core';
+
+@Component({
+  selector: '<%= selector %>',<% if(inlineTemplate) { %>
+  template: `
+    <p>
+      <%= dasherize(name) %> works!
+    </p>
+  `,<% } else { %>
+  templateUrl: './<%= dasherize(name) %>.html',<% if(!!viewEncapsulation) { %>,
+  encapsulation: ViewEncapsulation.<%= viewEncapsulation %><% } if (changeDetection !== 'Default') { %>,
+  changeDetection: ChangeDetectionStrategy.<%= changeDetection %><% } %>
+})
+export class <%= classify(name) %> implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
